d621b28d4d62858bcae8f76b496555d2a09409ec
shaun <northernlights_p@outlook.com>
2022-09-05 00:19:34 +0100

    _fix_ for script execution, hopefully. Added more verbose output during install + general cleanup

--- deck_script.sh
+++ deck_script.sh
@@ -3,7 +3,7 @@
 echo "Run as root!"
 
 while true; do
-    read -p "This will automatically install mpv, webtorrent, dmenu & notflix. Do you want to continue? " yn
+    read -p "This will automatically install mpv, webtorrent, dmenu, yay & notflix. Do you want to continue? " yn
     case $yn in
     [Yy]* ) break;;
     [Nn]* ) exit;;
@@ -11,8 +11,15 @@ while true; do
 esac
 done
 
-# Grabbing direct from arch repos
-sudo pacman -S mpv webtorrent dmenu
+echo "Installing pkgs; mpv, yay, dmenu & webtorrent... "
+
+# Grabbing pkgs from Arch repos, installs yay
+sudo pacman -S mpv yay dmenu &&
+    # Installs from AUR
+    yay -S webtorrent-cli &&
+	echo "Packages installed!
+	    Now fetching mpv.conf & installing... " &&
+		sleep 3s && echo "Done! Automatically installing notflix... "
 
 # Fetching mpv.conf from my dotfiles repo
 sudo curl -sL "https://gitlab.com/ayylmaonade/dotfiles/-/raw/main/config/mpv.conf" -o ~/.config/mpv/mpv.conf
@@ -20,4 +27,4 @@ sudo curl -sL "https://gitlab.com/ayylmaonade/dotfiles/-/raw/main/config/mpv.con
 # Installing notflix
 sudo curl -sL "https://raw.githubusercontent.com/ayylmaonade/notflix/master/notflix" -o /usr/local/bin/notflix && chmod +x /usr/local/bin/notflix
 
-echo "Installation complete!" && sleep
+echo "Installation complete! Run 'notflix' to use the script!"
